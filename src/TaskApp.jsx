46c710ff1fea05e9042319fe83d57b9c1f42c9e3
khan faysal <mdfaysalkhancse@gmail.com>
2024-05-01 09:26:12 +0600

    implement short way switch case statement

--- src/TaskApp.jsx
+++ src/TaskApp.jsx
@@ -59,24 +59,40 @@ function taskReducer(tasks, action) {
   // } else {
   //   throw Error("Unknown action" + action.type);
   // }
-  switch (action.type) {
-    case "addTask": {
-      return [...tasks, { id: nextId++, text: action.text, done: false }];
-    }
-    case "deleteTask": {
-      return tasks.filter((t) => t.id !== action.id);
-    }
-    case "changeTask": {
-      return tasks.map((t) => {
-        if (t.id === action.task.id) {
-          return action.task;
-        } else {
-          return t;
-        }
-      });
-    }
-    default: {
-      throw Error("Unknown action" + action.type);
-    }
+
+  // switch (action.type) {
+  //   case "addTask": {
+  //     return [...tasks, { id: nextId++, text: action.text, done: false }];
+  //   }
+  //   case "deleteTask": {
+  //     return tasks.filter((t) => t.id !== action.id);
+  //   }
+  //   case "changeTask": {
+  //     return tasks.map((t) => {
+  //       if (t.id === action.task.id) {
+  //         return action.task;
+  //       } else {
+  //         return t;
+  //       }
+  //     });
+  //   }
+  //   default: {
+  //     throw Error("Unknown action" + action.type);
+  //   }
+  // }
+
+  // shortcut of switch case
+  const handlers = {
+    addTask: () => [...tasks, { id: nextId++, text: action.text, done: false }],
+    deleteTask: () => tasks.filter((t) => t.id !== action.id),
+    changeTask: () =>
+      tasks.map((t) => (t.id === action.task.id ? action.task : t)),
+  };
+
+  const handler = handlers[action.type];
+  if (handler) {
+    return handler();
+  } else {
+    throw new Error("Unknown action" + action.type);
   }
 }
